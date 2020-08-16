8ff6b82df430711f0107ad06305060fda947ff3c
wangzhibin <zhibin.wang@ambow.com>
2020-08-16 22:45:18 +0800

    update

--- src/main/java/com/bgw/aop/proxy/cglib/CglibTest.java
+++ src/main/java/com/bgw/aop/proxy/cglib/CglibTest.java
@@ -1,5 +1,9 @@
 package com.bgw.aop.proxy.cglib;
 
+import com.bgw.aop.proxy.HelloService;
+import com.bgw.aop.proxy.HelloServiceImpl;
+import org.springframework.cglib.proxy.Enhancer;
+
 /**
  * CglibTest
  *
@@ -7,4 +11,14 @@ package com.bgw.aop.proxy.cglib;
  * @since 2020/8/15 18:29
  **/
 public class CglibTest {
+
+    public static void main(String[] args) {
+        // Enhancer实际属于CGLIB包的，也就是`net.sf.cglib.proxy.Enhancer`，Spring给搞到自己包里来管理了
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(HelloServiceImpl.class);
+        enhancer.setCallback(new MyMethodInterceptor());
+
+        HelloServiceImpl helloService = (HelloServiceImpl) enhancer.create();
+        helloService.sayHello();
+    }
 }
--- src/main/java/com/bgw/aop/proxy/cglib/MyMethodInterceptor.java
+++ src/main/java/com/bgw/aop/proxy/cglib/MyMethodInterceptor.java
@@ -1,10 +1,26 @@
 package com.bgw.aop.proxy.cglib;
 
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
 /**
- * MyMethodInceptor
+ * MyMethodInterceptor
+ * org.aopalliance.intercept包下也有一个
  *
  * @author wang
  * @since 2020/8/15 18:25
  **/
-public class MyMethodInterceptor {
+public class MyMethodInterceptor implements MethodInterceptor {
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+        System.out.println("before");
+        Object result = methodProxy.invokeSuper(o, args);
+        System.out.println("after");
+        return result;
+    }
+
+
 }
