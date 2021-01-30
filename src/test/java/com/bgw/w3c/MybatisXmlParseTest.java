d5cb405b68227c62b43bab97ac3efbd55d3edf37
wangzhibin <zhibin.wang@ambow.com>
2021-01-30 17:32:51 +0800

    update

--- src/test/java/com/bgw/w3c/MybatisXmlParseTest.java
+++ src/test/java/com/bgw/w3c/MybatisXmlParseTest.java
@@ -1,13 +1,21 @@
 package com.bgw.w3c;
 
+import com.sun.corba.se.spi.orb.PropertyParser;
 import com.sun.org.apache.xerces.internal.dom.DeferredAttrImpl;
 import com.sun.org.apache.xerces.internal.dom.DeferredNode;
+import com.sun.org.apache.xpath.internal.compiler.XPathParser;
 import org.junit.Before;
 import org.junit.Test;
+import org.w3c.dom.Attr;
+import org.w3c.dom.CharacterData;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
 
 /**
  * MybatisXmlParseTest
@@ -25,13 +33,74 @@ public class MybatisXmlParseTest {
     }
 
     @Test
-    public void parseRoot() throws Exception {
-        NodeList nls = document.getChildNodes();
-        System.out.println("nls.item(0) = " + nls.item(0));
-        NodeList nls2 = document.getElementsByTagName("mapper");
-        System.out.println("nls2 = " + nls2);
+    public void parseElement() throws Exception {
+        Element root = document.getDocumentElement();
+        String rootTagName = root.getTagName();
+        Attr namespaceAttr = root.getAttributeNode("namespace");
+        System.out.println("rootTagName = " + rootTagName);
+        System.out.println("namespaceAttr.getName() = " + namespaceAttr.getName());
+        System.out.println("namespaceAttr.getValue() = " + namespaceAttr.getValue());
+
+        System.out.println("root.getNodeName() = " + root.getNodeName());
+        System.out.println("root.getNodeValue() = " + root.getNodeValue());
+        System.out.println("root.getNodeType() = " + root.getNodeType());
+    }
+
+    @Test
+    public void parseNode() throws Exception {
+        Element root = document.getDocumentElement();
+        NodeList nodeList = root.getChildNodes();
+        System.out.println("nodeList.getLength() = " + nodeList.getLength());
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            System.out.println("======================{" + (i + 1) + "}======================");
+            Node item = nodeList.item(i);
+            System.out.println("item.getNodeName() = " + item.getNodeName());
+            System.out.println("item.getNodeValue() = " + item.getNodeValue());
+            System.out.println("item.getNodeType() = " + item.getNodeType());
+            System.out.println("item = " + item.getTextContent());
+
+        }
+
+//        Node secondNode = nodeList.item(1);
+//        NodeList secList = secondNode.getChildNodes();
+//        for (int i = 0; i < secList.getLength(); i++) {
+//            Node item = secList.item(i);
+//            System.out.println("item.getNodeName() = " + item.getNodeName());
+//            System.out.println("item.getNodeValue() = " + item.getNodeValue());
+//            System.out.println("item.getNodeType() = " + item.getNodeType());
+//        }
+
+    }
+
+    @Test
+    public void testQueryById() {
+        String id = "listAll";
+
+        Element root = document.getDocumentElement();
+        NodeList nodeList = root.getChildNodes();
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node item = nodeList.item(i);
+            if (item.getNodeType() == 1) {
+               // Attr itemAttr = item.get
 
 
-        System.out.println("nls2 = " + nls2.item(0).getNodeName());
+            }
+        }
+
+
+    }
+
+    @Test
+    public void testXPath() throws Exception {
+        Element root = document.getDocumentElement();
+        NodeList nodeList = root.getChildNodes();
+
+        XPathFactory xPathFactory = XPathFactory.newInstance();
+        XPath xpath = xPathFactory.newXPath();
+        Object data = xpath.evaluate("/mapper/select", document);
+        System.out.println("data = " + data);
+
     }
 }
